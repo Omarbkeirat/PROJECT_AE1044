3f4934f2500d73bf46853b06d0aaed73bbaab4f5
MohmmadAlattar <mohmmadalattar1999@gmail.com>
2023-05-31 19:49:51 +0300

    Add project files.

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace FastFood
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TextBoxUsername = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TextBoxPassword = new System.Windows.Forms.TextBox();
+            this.bt_Cancel = new System.Windows.Forms.Button();
+            this.bt_Login = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblwelcomsignin = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 30F);
+            this.label1.Location = new System.Drawing.Point(165, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 57);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "HELLO !";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Himalaya", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(137, 118);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 25);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Username *";
+            // 
+            // TextBoxUsername
+            // 
+            this.TextBoxUsername.Location = new System.Drawing.Point(142, 147);
+            this.TextBoxUsername.Margin = new System.Windows.Forms.Padding(4);
+            this.TextBoxUsername.Name = "TextBoxUsername";
+            this.TextBoxUsername.Size = new System.Drawing.Size(301, 24);
+            this.TextBoxUsername.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Himalaya", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(140, 210);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 25);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Password *";
+            // 
+            // TextBoxPassword
+            // 
+            this.TextBoxPassword.Location = new System.Drawing.Point(133, 239);
+            this.TextBoxPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.TextBoxPassword.Name = "TextBoxPassword";
+            this.TextBoxPassword.Size = new System.Drawing.Size(310, 24);
+            this.TextBoxPassword.TabIndex = 15;
+            this.TextBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // bt_Cancel
+            // 
+            this.bt_Cancel.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.bt_Cancel.Font = new System.Drawing.Font("MV Boli", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_Cancel.Location = new System.Drawing.Point(52, 463);
+            this.bt_Cancel.Margin = new System.Windows.Forms.Padding(4);
+            this.bt_Cancel.Name = "bt_Cancel";
+            this.bt_Cancel.Size = new System.Drawing.Size(94, 40);
+            this.bt_Cancel.TabIndex = 16;
+            this.bt_Cancel.Text = "Back";
+            this.bt_Cancel.UseVisualStyleBackColor = false;
+            this.bt_Cancel.Click += new System.EventHandler(this.bt_Cancel_Click);
+            // 
+            // bt_Login
+            // 
+            this.bt_Login.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.bt_Login.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ButtonFace;
+            this.bt_Login.Font = new System.Drawing.Font("MV Boli", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_Login.Location = new System.Drawing.Point(175, 323);
+            this.bt_Login.Margin = new System.Windows.Forms.Padding(4);
+            this.bt_Login.Name = "bt_Login";
+            this.bt_Login.Size = new System.Drawing.Size(184, 74);
+            this.bt_Login.TabIndex = 17;
+            this.bt_Login.Text = "Login";
+            this.bt_Login.UseVisualStyleBackColor = false;
+            this.bt_Login.Click += new System.EventHandler(this.bt_Login_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(37, 222);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(55, 52);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblwelcomsignin
+            // 
+            this.lblwelcomsignin.AutoSize = true;
+            this.lblwelcomsignin.Font = new System.Drawing.Font("MV Boli", 8F);
+            this.lblwelcomsignin.Location = new System.Drawing.Point(181, 80);
+            this.lblwelcomsignin.Name = "lblwelcomsignin";
+            this.lblwelcomsignin.Size = new System.Drawing.Size(167, 18);
+            this.lblwelcomsignin.TabIndex = 18;
+            this.lblwelcomsignin.Text = "Sign in to your account";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(37, 131);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(55, 54);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 19;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MV Boli", 8F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(21, 441);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(175, 18);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "If you not employee *";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 516);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lblwelcomsignin);
+            this.Controls.Add(this.bt_Login);
+            this.Controls.Add(this.bt_Cancel);
+            this.Controls.Add(this.TextBoxPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TextBoxUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LoginForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TextBoxUsername;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TextBoxPassword;
+        private System.Windows.Forms.Button bt_Cancel;
+        private System.Windows.Forms.Button bt_Login;
+        private System.Windows.Forms.Label lblwelcomsignin;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ StartForm.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace FastFood
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label_Login = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.Gold;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DarkGray;
+            this.button1.Font = new System.Drawing.Font("Gill Sans Ultra Bold Condensed", 20F);
+            this.button1.Location = new System.Drawing.Point(155, 242);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(264, 84);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "View Menu";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label_Login
+            // 
+            this.label_Login.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_Login.AutoSize = true;
+            this.label_Login.BackColor = System.Drawing.Color.Gold;
+            this.label_Login.Font = new System.Drawing.Font("Stencil", 20F);
+            this.label_Login.Location = new System.Drawing.Point(415, 494);
+            this.label_Login.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label_Login.Name = "label_Login";
+            this.label_Login.Size = new System.Drawing.Size(123, 40);
+            this.label_Login.TabIndex = 3;
+            this.label_Login.Text = "Log in";
+            this.label_Login.Click += new System.EventHandler(this.label_Login_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gold;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 20F);
+            this.label1.Location = new System.Drawing.Point(81, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(415, 47);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Welcome To Rrestaurant";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gold;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 10F);
+            this.label2.Location = new System.Drawing.Point(405, 462);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 22);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Login Employee *";
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FastFood.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(622, 594);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_Login);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "StartForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "StartForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label_Login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
